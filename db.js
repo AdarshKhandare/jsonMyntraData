319716a1c0f7169464733cef2284c99e2ae346eb
AdarshKhandare <kingadarsh313@gmail.com>
2022-05-09 23:01:43 +0530

    data

---
+++ db.js
@@ -0,0 +1,26 @@
+const men = require("./data/men");
+const women = require("./data/women");
+const cart = require("./data/cart");
+const topInfluencerExclusiveStyles = require("./data/topInfluencerExclusiveStyles");
+const newInTopBrands = require("./data/newInTopBrands");
+const dealsoOnLatestArival = require("./data/dealsoOnLatestArival");
+const GiftingCards = require("./data/GiftingCards");
+const categoriesToBag = require("./data/categoriesToBag");
+const topPicks = require("./data/topPicks");
+const bestOfMyntra = require("./data/bestOfMyntra");
+const dealsofthday = require("./data/dealsofthday");
+
+
+module.exports = {
+  men,
+  women,
+  cart,
+  newInTopBrands,
+  topInfluencerExclusiveStyles,
+  dealsofthday,
+  bestOfMyntra,
+  topPicks,
+  categoriesToBag,
+  GiftingCards,
+  dealsoOnLatestArival,
+};
